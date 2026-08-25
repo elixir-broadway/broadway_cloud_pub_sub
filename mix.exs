6f3e16cb771bc29a6c1199e0abb10368b5ca32bf
Rock <miguel.garcia@fcm.digital>
2026-08-25 17:31:17 +0200

    2.0.0-rc.1 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BroadwayCloudPubSub.MixProject do
   use Mix.Project
 
-  @version "2.0.0-rc.0"
+  @version "2.0.0-rc.1"
   @description "A Google Cloud Pub/Sub connector for Broadway"
   @repo_url "https://github.com/dashbitco/broadway_cloud_pub_sub"
 
